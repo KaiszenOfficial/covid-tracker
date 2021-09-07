ae5085c5687c41ac5c24b0cb2956201b2463ec8f
Supratik <supratik1992@gmail.com>
2021-09-07 16:03:29 +0530

    dark and light theme added based on users system preference

--- src/App.js
+++ src/App.js
@@ -1,20 +1,59 @@
-import "./App.css";
-import { Typography } from "@material-ui/core";
-import { Cards, Chart, InputSelector } from "./components";
-import { useEffect, useState } from "react";
-import { fetchData } from "./api";
-import covidImage from "./images/covid19_logo.png";
+import './App.css';
+import {
+  createTheme,
+  Typography,
+  ThemeProvider,
+  CssBaseline,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from '@material-ui/core';
+import { Cards, Chart, InputSelector } from './components';
+import { useEffect, useState } from 'react';
+import { fetchData } from './api';
+import covidImage from './images/covid19_logo.png';
+
+const baseTheme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat'
+  }
+})
+
+const darkTheme = createTheme({
+  ...baseTheme,
+  palette: {
+    type: 'dark',
+  },
+});
+
+const lightTheme = createTheme({
+  ...baseTheme,
+  palette: {
+    type: 'light',
+  },
+});
 
 function App() {
-
+  const [theme, setTheme] = useState(lightTheme);
   const [data, setData] = useState({});
   const [country, setCountry] = useState('');
 
   useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)')
+    ) {
+      setTheme(darkTheme);
+    } else {
+      setTheme(lightTheme);
+    }
+  }, []);
+
+  useEffect(() => {
     const fetchDataFromApi = async () => {
       const apiData = await fetchData();
-      setData(apiData)
-    }
+      setData(apiData);
+    };
 
     fetchDataFromApi();
   }, []);
@@ -23,16 +62,31 @@ function App() {
     const apiData = await fetchData(country);
     setCountry(country);
     setData(apiData);
-  }
+  };
 
   return (
-    <div className="container">
-      <img className="image" src={covidImage} alt="Covid-19" />
-      <Typography variant="h5" style={{ marginBottom: "1.5rem" }}>Covid 19 Tracker</Typography>
-      <InputSelector country={country} handleCountryChange={handleCountryChange} />
-      <Cards data={data} />
-      <Chart data={data} country={country} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton>
+            <img className="image" src={covidImage} alt="Covid-19" />
+          </IconButton>
+          <Typography variant="h5">Covid 19 Tracker</Typography>
+        </Toolbar>
+      </AppBar>
+      <div className="container">
+        {/* <Typography variant="h5" style={{ marginBottom: '1.5rem' }}>
+          Covid 19 Tracker
+        </Typography> */}
+        <InputSelector
+          country={country}
+          handleCountryChange={handleCountryChange}
+        />
+        <Cards data={data} />
+        <Chart data={data} country={country} />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Grid, Card, CardContent, Typography } from "@material-ui/core";
-import CountUp from "react-countup";
-import "./Cards.css";
+import React from 'react';
+import { Grid, Card, CardContent, Typography } from '@material-ui/core';
+import CountUp from 'react-countup';
+import './Cards.css';
 
 export default function Cards({
   data: { confirmed, recovered, deaths, lastUpdate },
@@ -11,60 +11,66 @@ export default function Cards({
   }
   return (
     <div className="card-container">
-      <Grid container spacing={0} justifyContent="center">
-        <Grid item component={Card} xs={12} md={3} className="card infected">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Active
-            </Typography>
-            <Typography variant="h5">
-              <CountUp
-                start={0}
-                end={confirmed.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-          </CardContent>
+      <Grid container spacing={3} justifyContent="center">
+        <Grid item xs={12} md={3} >
+          <Card className="infected">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Active
+              </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={confirmed.value}
+                  duration={2.5}
+                  separator=","
+                />
+              </Typography>
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className="card recovered">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Recovered
-            </Typography>
-            <Typography variant="h5">
-              <CountUp
-                start={0}
-                end={recovered.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-          </CardContent>
+        <Grid item xs={12} md={3}>
+          <Card className="recovered">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Recovered
+              </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={recovered.value}
+                  duration={2.5}
+                  separator=","
+                />
+              </Typography>
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className="card deaths">
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Fatalities
-            </Typography>
-            <Typography variant="h5">
-              <CountUp
-                start={0}
-                end={deaths.value}
-                duration={2.5}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">
-              {new Date(lastUpdate).toDateString()}
-            </Typography>
-          </CardContent>
+        <Grid item xs={12} md={3}>
+          <Card className="deaths">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Fatalities
+              </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={deaths.value}
+                  duration={2.5}
+                  separator=","
+                />
+              </Typography>
+              <Typography color="textSecondary">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </div>
